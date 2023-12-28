b5578549e08f7a7d857daa4d2f18295e8aec9240
Tatiana Lin <tatianalin@MacBook-Air-de-Tatiana-6.local>
2023-12-28 15:57:57 +0000

    just added a cmake.txt bc it wasnt working in my pc

---
+++ cmake-build-debug/CMakeFiles/airport.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/airport.dir/Users/tatianalin/CLionProjects/airport/main.cpp.o"
+  "CMakeFiles/airport.dir/Users/tatianalin/CLionProjects/airport/main.cpp.o.d"
+  "airport"
+  "airport.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/airport.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
